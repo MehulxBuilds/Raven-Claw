a710978f49564b4ac011c2f18c44a4471139928f
Mehul Prajapati <ailearn90@gmail.com>
2026-03-18 20:28:04 +0530

    feat: added auth shared package again

---
+++ packages/auth/src/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { client } from "@repo/db";
+import { server_env as env } from "@repo/env";
+
+export const auth = betterAuth({
+    database: prismaAdapter(client, {
+        provider: "postgresql",
+    }),
+
+    trustHost: true,                   // 👈 REQUIRED FOR DEV
+    secret: env.BETTER_AUTH_SECRET,
+
+    socialProviders: {
+        github: {
+            clientId: env.GITHUB_CLIENT_ID!,
+            clientSecret: env.GITHUB_CLIENT_SECRET!,
+        }
+    }
+});
